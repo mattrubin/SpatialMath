c3547c26674e6ad8c2959b76d2c3ae65186a8632
Matt Rubin <mattrubin@me.com>
2011-01-24 19:00:59 -0800

    Added SpatialMath.h; made headers public

---
+++ SpatialMath.h
@@ -0,0 +1,11 @@
+//
+//  SpatialMath.h
+//  SpatialMath
+//
+//  Created by Matt Rubin on 1/22/11.
+//  Copyright 2011 Stanford University. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "Matrix.h"
