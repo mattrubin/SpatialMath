11752557856ca971c8549dbe51d38b77c1e80f92
Matt Rubin <mattrubin@me.com>
2011-04-07 15:21:14 -0700

    [minor] Added Test_Vector stub

---
+++ Tests/Test_Vector.h
@@ -0,0 +1,17 @@
+//
+//  Test_Vector.h
+//  SpatialMath
+//
+//  Created by Matt Rubin on 4/7/11.
+//  Copyright 2011 Matt Rubin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface Test_Vector : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ Tests/Test_Vector.m
@@ -0,0 +1,28 @@
+//
+//  Test_Vector.m
+//  SpatialMath
+//
+//  Created by Matt Rubin on 4/7/11.
+//  Copyright 2011 Matt Rubin. All rights reserved.
+//
+
+#import "Test_Vector.h"
+
+
+@implementation Test_Vector
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+@end
