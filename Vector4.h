317b60e78aa69415dddb55ff8064849329bd7d73
Matt Rubin <mattrubin@me.com>
2011-07-06 21:23:57 -0400

    Added Vector4

---
+++ Vector4.h
@@ -0,0 +1,20 @@
+//
+//  Vector4.h
+//  SpatialMath
+//
+//  Created by Matt Rubin on 7/6/11.
+//  Copyright 2011 Matt Rubin. All rights reserved.
+//
+
+#pragma once
+#import "Vector.h"
+
+
+@interface Vector4 : Vector
+
+@property (assign) double x;
+@property (assign) double y;
+@property (assign) double z;
+@property (assign) double w;
+
+@end
---
+++ Vector4.m
@@ -0,0 +1,57 @@
+//
+//  Vector4.m
+//  SpatialMath
+//
+//  Created by Matt Rubin on 7/6/11.
+//  Copyright 2011 Matt Rubin. All rights reserved.
+//
+
+#import "Vector4.h"
+
+
+@implementation Vector4
+
+#pragma mark Initializers
+- (id)init
+{
+	if ((self = [super init])) {
+		rows = 3;
+		elements = (double*)calloc(rows*columns, sizeof(double));
+	}
+	return self;
+}
+
+#pragma mark Setters and Getters
+- (double)x {
+	return elements[0];
+}
+
+- (double)y {
+	return elements[1];
+}
+
+- (double)z {
+	return elements[2];
+}
+
+- (double)w {
+	return elements[3];
+}
+
+- (void)setX:(double)x {
+	elements[0] = x;
+}
+
+- (void)setY:(double)y {
+	elements[1] = y;
+}
+
+- (void)setZ:(double)z {
+	elements[2] = z;
+}
+
+- (void)setW:(double)w {
+	elements[3] = w;
+}
+
+@end
